e18cfcdd509d1b479667ad2c79b5c13f27672a8e
Navneet9968 <106442750+Navneet9968@users.noreply.github.com>
2023-08-21 12:46:28 +0530

    Delete src directory

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
-});
-
-//whenever we try instance.get('/something'),it will try to go to https://api.themoviedb.org/3/something
-
-export default instance;
